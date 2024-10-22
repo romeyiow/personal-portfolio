aa304ed56fc8946c1938341a5c9434dc46a724c2
romeyiow <jimperialyosh@gmail.com>
2024-10-23 02:50:42 +0800

    Addded design of hero and nav

--- src/App.jsx
+++ src/App.jsx
@@ -11,17 +11,26 @@ import Footer from "./components/Footer"
 function App() {
   return (
     <div className="pseudo-body">
-      <Navbar/>
-      <HeroSection/>
-      <StatsSection />
-      <BioSection/>
-      <SkillsSection/>
-      <ProjectsSection/>
-      <ReviewsSection/>
-      <ContactSection/>
-      <Footer/>
+      <div className="designs-container">
+        <div className="nav-hero-des left">
+          <img src="public\assets\images\design\three-clustered-dots.png" alt="" />
+        </div>
+        <div className="nav-hero-des right">
+          <img src="public\assets\images\design\Group 9079.png" alt="" />
 
-      
+        </div>
+      </div>
+      <Navbar />
+      <HeroSection />
+      <StatsSection />
+      <BioSection />
+      <SkillsSection />
+      <ProjectsSection />
+      <ReviewsSection />
+      <ContactSection />
+      <Footer />
+
+
     </div>
 
   )
