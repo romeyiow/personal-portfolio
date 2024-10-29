17141ce8f1ccca59c7d9c98a01a71de662cfbdf6
romeyiow <jimperialyosh@gmail.com>
2024-10-29 20:01:16 +0800

    Done updating project section

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,47 +1,47 @@
-export default function HeroSection(){
-    return (
-        <section className="hero" id="hero">
-        <div className="texts">
-          <span className="elli-marks">Welcome</span>
-          <h1 className="">
-            Avid <span>Learner</span>. Problem
-            <span>Solver</span>.  <span>Ready</span> to serve!
-          </h1>
-          <p className="desc">
-            Hi! I am Jers, a full stack web developer. I have been helping
-            businesses solve their problems with my creative works for more than 4
-            years now.{" "}
-          </p>
-          <div className="cta-btns">
-            <a className="hire" href="#contact">
-              Hire Me
+export default function HeroSection() {
+  return (
+    <section className="hero" id="hero">
+      <div className="texts">
+        <span className="elli-marks">Welcome</span>
+        <h1 className="">
+          Avid <span>Learner</span>. Problem
+          <span>Solver</span>.  <span>Ready</span> to serve!
+        </h1>
+        <p className="desc">
+          Hi! I am Jers, a full stack web developer. I have been helping
+          businesses solve their problems with my creative works for more than 4
+          years now.{" "}
+        </p>
+        <div className="cta-btns">
+          <a className="hire" href="#contact">
+            Hire Me
+          </a>
+          <a className="view-proj" href="#projects">
+            View Projects
+            {/* <img src="assets/icons/go-to-arrow.png" alt="" /> */}
+          </a>
+        </div>
+      </div>
+      <div className="graphics">
+        <div className="wrapper-dp">
+          <img className="dp" src="assets/images/hero/dp.png" alt="" />
+        </div>
+        <div className="socmed">
+          <img src="assets/icons/Follow me on_.png" alt="" />
+          <span className="line" />
+          <div className="wrapper-imgs">
+            <a href="https://www.facebook.com/rmeitaly/" target="_blank">
+              <img src="assets/icons/facebook.png" alt="" />
             </a>
-            <a className="view-proj" href="#projects">
-              View Projects 
-              {/* <img src="assets/icons/go-to-arrow.png" alt="" /> */}
+            <a href="https://www.instagram.com/romeyiow/" target="_blank">
+              <img src="assets/icons/instagram.png" alt="" />
+            </a>
+            <a href="https://www.linkedin.com/in/thereliablerome" target="_blank">
+              <img src="assets/icons/linkedin.png" alt="" />
             </a>
           </div>
         </div>
-        <div className="graphics">
-          <div className="wrapper-dp">
-            <img className="dp" src="assets/images/hero/dp.png" alt="" />
-          </div>
-          <div className="socmed">
-            <img src="assets/icons/Follow me on_.png" alt="" />
-            <span className="line" />
-            <div className="wrapper-imgs">
-              <a href="">
-                <img src="assets/icons/facebook.png" alt="" />
-              </a>
-              <a href="">
-                <img src="assets/icons/instagram.png" alt="" />
-              </a>
-              <a href="">
-                <img src="assets/icons/linkedin.png" alt="" />
-              </a>
-            </div>
-          </div>
-        </div>
-      </section>
-    )
+      </div>
+    </section>
+  )
 }
--- src/components/ProjectsSection.jsx
+++ src/components/ProjectsSection.jsx
@@ -1,4 +1,40 @@
+import React, { useState } from 'react';
+
 export default function ProjectsSection() {
+    const [counter, setCounter] = useState(0);
+    const [fadeClass, setFadeClass] = useState('');
+
+    const arrayOfImages = [
+        "assets/images/projects/donation-website.png",
+        "assets/images/projects/portfolio-website.png",
+        "assets/images/projects/blue.png"
+    ];
+
+    const arrayOfLinks = [
+        "https://jcidonationwebsite.netlify.app/home",
+        "https://leafy-mochi-001fb8.netlify.app/",
+        "https://github.com/romeyiow/romeyiow"
+    ];
+    const arrayOfTitles = [
+        "Responsive Donation Website",
+        "Simple Portfolio Website",
+        "Web Development"
+    ];
+    const arrayOfDescs = [
+        "Check out this responsive website donation website.",
+        "Here's the first look of my personal portfolio website.",
+        "Check out my these websites I have created over the past 4 years."
+    ];
+
+    const changeBackgroundImage = (k) => {
+        setFadeClass('fade-out');
+        setTimeout(() => {
+            const newCounter = (counter + k + 3) % 3;
+            setCounter(newCounter);
+            setFadeClass('fade-in');
+        }, 300);
+    };
+
     return (
         <section className="projects" id="projects">
             <div className="texts">
@@ -10,20 +46,31 @@ export default function ProjectsSection() {
                 <p className="desc">
                     With a proven track record of delivering high-quality results and a
                     commitment to meeting deadlines, I bring your revolutionary vision to
-                    ife.
+                    life.
                 </p>
                 <div className="cta-btn">
-                    <a className="contact" href="">
+                    <a className="contact" href="https://github.com/romeyiow/romeyiow" target="_blank" rel="noopener noreferrer">
                         See More
                     </a>
                 </div>
             </div>
             <div className="projects-visuals">
-                <div className="item highlight"></div>
+                <div className="item highlight-wrapper">
+                    <div className="item highlight" style={{ backgroundImage: `url(${arrayOfImages[counter]})` }}></div>
+                    <div className="highlight-info">
+                        <a className={`project-link ${fadeClass}`} href={arrayOfLinks[counter]} target="_blank" rel="noopener noreferrer"><img src="" alt="" /><img src="assets/images/projects/External-Link.png" alt="" /></a>
+                        <h5 className={`project-title ${fadeClass}`}>
+                            {arrayOfTitles[counter]}
+                        </h5>
+                        <p className={`project-short-desc ${fadeClass}`}>
+                            {arrayOfDescs[counter]}
+                        </p>
+                    </div>
+                </div>
                 <div className="item slide-show">
-                    <div className="slide-card top-image" />
-                    <div className="slide-card mid-image" />
-                    <div className="slide-card bottom-image" />
+                    <div className="slide-card top-image" style={{ backgroundImage: `url(${arrayOfImages[(counter + 1) % 3]})` }} />
+                    <div className="slide-card mid-image" style={{ backgroundImage: `url(${arrayOfImages[(counter + 2) % 3]})` }} />
+                    <div className="slide-card bottom-image" style={{ backgroundImage: `url(${arrayOfImages[(counter + 3) % 3]})` }} />
                     <div className="controls">
                         <div className="indicators">
                             <div className="box-wrapper active-wrapper" id="wrapper1">
@@ -58,6 +105,5 @@ export default function ProjectsSection() {
                 </div>
             </div>
         </section>
-
-    )
+    );
 }
